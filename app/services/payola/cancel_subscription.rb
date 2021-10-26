b46853e48b5d18b8737e3e52edb5c0fd206ff577
Joel Oliveira <joel.oliveira@gmail.com>
2021-10-25 21:07:18 -0400

    Use `update` instead of `update_attributes`
    Rails: `Deprecate update_attributes and update_attributes!`
    
    https://github.com/rails/rails/pull/31998
    
    So ... all of these `update_attributes` need to change to `update`

--- app/services/payola/cancel_subscription.rb
+++ app/services/payola/cancel_subscription.rb
@@ -4,10 +4,10 @@ module Payola
       secret_key = Payola.secret_key_for_sale(subscription)
       customer = Stripe::Customer.retrieve(subscription.stripe_customer_id, secret_key)
       customer.subscriptions.retrieve(subscription.stripe_id, secret_key).delete(options, secret_key)
-      
+
       if options[:at_period_end] == true
         # Store that the subscription will be canceled at the end of the billing period
-        subscription.update_attributes(cancel_at_period_end: true)
+        subscription.update(cancel_at_period_end: true)
       else
         # Cancel the subscription immediately
         subscription.cancel!
